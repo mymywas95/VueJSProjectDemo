a43d1b86c711d7c8bcbffa331d589526b33d118f
mymywas <mynvt95@gmail.com>
2018-01-31 00:36:48 +0700

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button v-on:click="reverseMessage">Reverse Message</button>
+    <input v-model="msg">
+  </div>
+</template>
+
+<script>
+  import todo from './ToDo.vue';
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    methods: {
+      reverseMessage: function () {
+        this.msg = this.msg.split('').reverse().join('')
+      }
+    },
+    components: {
+      'todo': todo
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'todo',
+    data () {
+      return {
+        msg: 'Welcome to todo component'
+      }
+    },
+  }
+</script>
